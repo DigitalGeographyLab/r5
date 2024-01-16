0df22f5d36e1038fcad3046185c826dde4ccbf13
Christoph Fink <christoph.fink@gmail.com>
2024-01-16 17:21:10 +0200

    changed method name in AnalysisWorkerTask

--- src/test/java/com/conveyal/r5/customcost/CustomCostTest.java
+++ src/test/java/com/conveyal/r5/customcost/CustomCostTest.java
@@ -114,7 +114,7 @@ public class CustomCostTest {
         // build the task from the grid, example taken from SimpsonDesertTests.java
         AnalysisWorkerTask task = gridLayout.newTaskBuilder()
                 .setOrigin(2, 2)
-                .setDestination(5, 3)
+                .singleFreeformDestination(5, 3)
                 .uniformOpportunityDensity(2)
                 .monteCarloDraws(1)
                 .build();
@@ -278,7 +278,7 @@ public class CustomCostTest {
         // build the task from the grid, example taken from SimpsonDesertTests.java
         AnalysisWorkerTask task = gridLayout.newTaskBuilder()
                 .setOrigin(2, 2)
-                .setDestination(5, 3)
+                .singleFreeformDestination(5, 3)
                 .uniformOpportunityDensity(2)
                 .monteCarloDraws(1)
                 .build();
